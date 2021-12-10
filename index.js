4ceda379ca9615a2786d0e11f1b1c5164819266b
Melissa Hale <melissakayh@gmail.com>
2021-12-10 16:06:00 -0600

    test env vars

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("and another thing"));
+app.get("/", (req, res) => res.send("ch ch chaaanges"));
 
 app.get("/about", (req, res) => res.send("About Page Route"));
 
